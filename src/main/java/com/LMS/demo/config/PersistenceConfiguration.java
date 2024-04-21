0cc951d0a7946862e5ffe2898d6ee33394ab7bd7
Singhal <akshat.singhal@sap.com>
2024-04-21 19:05:40 +0530

    second commit

--- src/main/java/com/LMS/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/LMS/demo/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-	@Bean
-	public DataSource dataSource() {
-		DataSourceBuilder builder = DataSourceBuilder.create();
-		builder.url("jdbc:postgresql://localhost:5433/DEMO");
-		builder.username("postgres");
-		builder.password("postgres");
-		System.out.println("My custom datasource bean has been initialized and set");
-		return builder.build();
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DataSourceBuilder builder = DataSourceBuilder.create();
+//		builder.url("jdbc:postgresql://localhost:5433/DEMO");
+//		builder.username("postgres");
+//		builder.password("postgres");
+//		System.out.println("My custom datasource bean has been initialized and set");
+//		return builder.build();
+//	}
 }
